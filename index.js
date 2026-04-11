c0477baee2e876fbde3920bb5bc9e175a5a3c7d7
student-nithin <ninsai2007@gmail.com>
2026-04-11 10:38:59 +0530

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import env from "dotenv"
+env.config();
 
 const app = express();
 const port = 3000;
@@ -9,7 +11,7 @@ const db = new pg.Client({
     user: "postgres",
     host: "localhost",
     database: "blog",
-    password: "NithiN02-04P*$tgr@$",
+    password: process.env.DATABASE_PASSWORD,
     port: 5432
 });
 db.connect();
